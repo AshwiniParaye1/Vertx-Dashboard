72986346f24149cf9058313d294de980e8c96a09
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-05 20:37:22 +0530

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ const Header = ({ activeSection }) => {
   return (
     <div className="flex justify-between items-center border-b border-gray-800 px-6 py-4">
       <div>
-        <span className="text-xl">
+        <span className="text-base font-semibold">
           {activeSection.charAt(0).toUpperCase() + activeSection.slice(1)}
         </span>
       </div>
-      <div className="flex items-center">
-        <button className="px-4 py-2 mr-4">Activity</button>
-        <button className="px-4 py-2">Log out</button>
+      <div className="flex items-center text-base font-semibold">
+        <button className="px-4 py-2 mr-4 ">Activity</button>
+        <button className="px-4 py-2 ">Log out</button>
       </div>
     </div>
   );
