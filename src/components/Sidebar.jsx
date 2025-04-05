8acb37403467f2163462c5a4390d074ab5dfe772
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-04-05 16:11:47 +0530

    added border

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
       {/* Logo and User */}
       <div className="p-4 border-gray-800">
         {/* Logo */}
-        <div className="flex items-center mb-8  border-gray-800">
+        <div className="flex items-center mb-6  border-gray-800">
           <div className="bg-white p-2 rounded-full mr-3">
             <FiFilter className="text-black" />
           </div>
@@ -14,26 +14,28 @@ const Sidebar = () => {
         </div>
 
         {/* User and Navigation */}
-        <div className="flex items-start">
+        <div className="flex items-start border-t border-gray-800">
           {/* User */}
-          <div className="w-10 h-10 rounded-full mr-3 overflow-hidden relative">
-            <img
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt="User avatar"
-              className="object-cover w-full h-full"
-            />
-            <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-black"></div>
+          <div className="border-r border-b p-2 border-gray-800 mt-2">
+            <div className="w-10 h-10 rounded-full mr-3 overflow-hidden relative">
+              <img
+                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                alt="User avatar"
+                className="object-cover w-full h-full"
+              />
+              <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-black"></div>
+            </div>
           </div>
 
           {/* Navigation */}
           <nav className="flex-1">
             <ul>
-              <li className="px-4 py-2 text-white font-medium">Dashboard</li>
-              <li className="px-4 py-2 text-white font-medium">Analytics</li>
-              <li className="px-4 py-2 text-gray-500">Connect</li>
-              <li className="px-4 py-2 text-gray-500">Dealroom</li>
-              <li className="px-4 py-2 text-gray-500">Profile</li>
-              <li className="px-4 py-2 text-gray-500">Settings</li>
+              <li className="px-4 py-4 text-white font-medium">Dashboard</li>
+              <li className="px-4 py-4 text-white font-medium">Analytics</li>
+              <li className="px-4 py-4 text-gray-500">Connect</li>
+              <li className="px-4 py-4 text-gray-500">Dealroom</li>
+              <li className="px-4 py-4 text-gray-500">Profile</li>
+              <li className="px-4 py-4 text-gray-500">Settings</li>
             </ul>
           </nav>
         </div>
